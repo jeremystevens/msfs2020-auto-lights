511180b06f354fc765946d2f359c06e0175c05e2
Jeremy Stevens <jeremiahstevens@gmail.com>
2023-04-03 16:26:49 -0700

    add license info to the bottom of the files

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from enum import Enum, auto
 from msfs_lights import AirplaneLights, get_airplane_light_values, set_airplane_light_value, get_airplane_altitude, is_night, is_on_ground
 import logging
 
+# ---- See License at bottom of file ----
+
 # logging
 # Configure the logger to save messages to a file
 logging.basicConfig(
@@ -88,3 +90,31 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# --------- license info ---------------------------------------------------#
+
+#MIT License
+
+#Copyright (c) 2023 - Jeremy Stevens
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+# -------------------- end of license info ---------------------------------------
